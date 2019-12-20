216b96491d9ba77d050bff65da20e3c8d555114e
Alexander Zaslonov <zaslonov.alexander@gmail.com>
2019-12-20 15:26:34 -0800

    tmp

--- src/apim.runtime.module.ts
+++ src/apim.runtime.module.ts
@@ -61,7 +61,7 @@ import { VisibilityGuard } from "@paperbits/common/user";
 import { StaticUserService } from "./services";
 import { SignOutRouteGuard } from "./routing/signOutRouteGuard";
 import { ProvisionService } from "./services/provisioningService";
-import { ClickCounterRuntimeModule } from "../examples/widget/click-counter/clickCounter.runtime.module";
+import { ClickCounterRuntimeModule } from "../examples/widget/click-counter";
 
 export class ApimRuntimeModule implements IInjectorModule {
     public register(injector: IInjector): void {
