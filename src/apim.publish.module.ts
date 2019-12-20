a5cd94ae34e5f560afb9df2572ef962bb894d31a
Alexander Zaslonov <zaslonov.alexander@gmail.com>
2019-12-19 18:10:24 -0800

    Cleanup.

--- src/apim.publish.module.ts
+++ src/apim.publish.module.ts
@@ -32,7 +32,7 @@ import { ValidationSummaryModule } from "./components/users/validation-summary/k
 import { BackendService } from "./services/backendService";
 import { StaticRoleService } from "./services/roleService";
 import { ProvisionService } from "./services/provisioningService";
-import { ClickCounterPublishModule } from "../examples/widget/click-counter/ko";
+import { ClickCounterPublishModule } from "../examples/widget/click-counter";
 
 export class ApimPublishModule implements IInjectorModule {
     public register(injector: IInjector): void {
