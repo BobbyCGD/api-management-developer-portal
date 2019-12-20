9aa03bd7d135b36c002e7fe7e3574a5e6001eb52
Alexander Zaslonov <zaslonov.alexander@gmail.com>
2019-12-19 18:08:07 -0800

    Cleanup.

--- examples/widget/click-counter/index.ts
+++ examples/widget/click-counter/index.ts
@@ -1,3 +1,6 @@
 export * from "./clickCounterHandlers";
 export * from "./clickCounterModel";
 export * from "./clickCounterModelBinder";
+export * from "./clickCounter.design.module";
+export * from "./clickCounter.publish.module";
+export * from "./clickCounter.runtime.module";
--- examples/widget/click-counter/ko/index.ts
+++ examples/widget/click-counter/ko/index.ts
@@ -1,5 +1,3 @@
-export * from "../clickCounter.publish.module";
-export * from "../clickCounter.design.module";
 export * from "./clickCounterEditor";
 export * from "./clickCounterViewModel";
 export * from "./clickCounterViewModelBinder";
