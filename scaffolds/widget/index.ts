22910eb3dfea223324ad19e63780751e04d10512
Alexander Zaslonov <zaslonov.alexander@gmail.com>
2020-01-10 17:21:47 -0800

    tmp

--- scaffolds/widget/index.ts
+++ scaffolds/widget/index.ts
@@ -2,6 +2,3 @@ export * from "./constants";
 export * from "./widgetHandlers";
 export * from "./widgetModel";
 export * from "./widgetModelBinder";
-export * from "./widget.design.module";
-export * from "./widget.publish.module";
-export * from "./widget.runtime.module";
